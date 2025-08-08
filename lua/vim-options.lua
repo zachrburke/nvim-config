ca7d9b3b801a59ee16df31fd727b36f611fd64cc
Zach Burke <zach.r.burke@gmail.com>
2025-08-08 01:29:09 -0500

    plugins for, telescope, treesitter, completions, and lsp

---
+++ lua/vim-options.lua
@@ -0,0 +1,8 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.g.mapleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
