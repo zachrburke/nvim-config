08e1772feef3da9b8a985cc429dd367f36b55619
Zach Burke <zach.r.burke@gmail.com>
2025-08-10 00:04:51 -0400

    adding back vgit

---
+++ lua/plugins/git.lua
@@ -0,0 +1,25 @@
+return {
+    'tanvirtin/vgit.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
+    -- Lazy loading on 'VimEnter' event is necessary.
+    event = 'VimEnter',
+    config = function() 
+        require('vgit').setup({
+            keymaps = {
+                ['n <C-k>'] = function() require('vgit').hunk_up() end,
+                {
+                    mode = 'n',
+                    key = '<C-j>',
+                    handler = 'hunk_down',
+                    desc = 'Go down in the direction of the hunk',
+                },
+                ['n <leader>gp'] = function() require('vgit').buffer_hunk_preview() end,
+                ['n <leader>gf'] = function() require('vgit').buffer_diff_preview() end,
+                ['n <leader>gh'] = function() require('vgit').buffer_history_preview() end,
+                ['n <leader>gu'] = function() require('vgit').buffer_reset() end,
+                ['n <leader>gd'] = function() require('vgit').project_diff_preview() end,
+                ['n <leader>gx'] = function() require('vgit').toggle_diff_preference() end,
+            }
+        })
+    end,
+}
