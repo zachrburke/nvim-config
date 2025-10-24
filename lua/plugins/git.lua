d77d8fd7ecf524e84c679b1878c30e0f3b13dece
Zach Burke <zach.r.burke@gmail.com>
2025-10-23 23:48:36 -0500

    Trying out new git plugin

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,25 +1,13 @@
 return {
-    'tanvirtin/vgit.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
-    -- Lazy loading on 'VimEnter' event is necessary.
-    event = 'VimEnter',
-    config = function() 
-        require('vgit').setup({
-            keymaps = {
-                ['n <leader>gk'] = function() require('vgit').hunk_up() end,
-                {
-                    mode = 'n',
-                    key = '<leader>gj',
-                    handler = 'hunk_down',
-                    desc = 'Go down in the direction of the hunk',
-                },
-                ['n <leader>gp'] = function() require('vgit').buffer_hunk_preview() end,
-                ['n <leader>gf'] = function() require('vgit').buffer_diff_preview() end,
-                ['n <leader>gh'] = function() require('vgit').buffer_history_preview() end,
-                ['n <leader>gu'] = function() require('vgit').buffer_reset() end,
-                ['n <leader>gd'] = function() require('vgit').project_diff_preview() end,
-                ['n <leader>gx'] = function() require('vgit').toggle_diff_preference() end,
-            }
-        })
-    end,
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",         -- required
+    "sindrets/diffview.nvim",        -- optional - Diff integration
+
+    -- Only one of these is needed.
+    "nvim-telescope/telescope.nvim", -- optional
+    "ibhagwan/fzf-lua",              -- optional
+    "nvim-mini/mini.pick",           -- optional
+    "folke/snacks.nvim",             -- optional
+  },
 }
