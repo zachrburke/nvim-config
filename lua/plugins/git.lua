96ed192e1b6ca3d9085b5f55f5699af345cc1aca
Zach Burke <zach.r.burke@gmail.com>
2025-10-24 12:38:41 -0500

    Diffview macros, adding gitsigns

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,10 +1,23 @@
 return {
     {
+        "lewis6991/gitsigns.nvim"
+    },
+    {
         "NeogitOrg/neogit",
         dependencies = {
             "nvim-lua/plenary.nvim",         -- required
             "sindrets/diffview.nvim",        -- optional - Diff integration
             "nvim-telescope/telescope.nvim", -- optional
         },
+        config = function ()
+            vim.keymap.set('n', '<leader>gd', ':DiffviewOpen<cr>')
+            require("diffview").setup({
+              keymaps = {
+                  file_panel = {
+                        {"n", "<leader>q", ":DiffviewClose<cr>", { desc = "Close the freakin' diffview"}},
+                  },
+              },
+          })
+        end
     }
 }
