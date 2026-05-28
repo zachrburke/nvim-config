e16f460f8e101e8b7a3874be8134645077552953
Zach Burke <zach.r.burke@gmail.com>
2026-05-28 11:36:28 -0500

    Updating nvim-treesitter to main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -11,29 +11,48 @@ return {
     },
     {
         "nvim-treesitter/nvim-treesitter",
-        dependencies = {
-            "windwp/nvim-ts-autotag",
-            "nvim-treesitter/nvim-treesitter-textobjects",
-        },
+        branch = "main",
+        lazy = false,
         build = ":TSUpdate",
         config = function()
-            local config = require("nvim-treesitter.configs")
-            config.setup({
-                auto_install = true,
-                highlight = { enable = true },
-                indent = { enable = true },
-                textobjects = {
-                    select = {
-                        enable = true,
-                        lookahead = true,
-                        keymaps = {
-                            ["af"] = "@function.outer", -- select function including signature
-                            ["if"] = "@function.inner", -- select function body only
-                        },
-                    },
-                },
+            local ensure_installed = {
+                "lua", "vim", "vimdoc", "query",
+                "javascript", "typescript", "tsx",
+                "html", "css", "json", "yaml",
+                "bash", "markdown", "markdown_inline",
+                "rust", "go", "python", "c", "cpp",
+            }
+            require("nvim-treesitter").install(ensure_installed)
+
+            vim.api.nvim_create_autocmd('FileType', {
+                pattern = { '<filetype>' },
+                callback = function()
+                    vim.treesitter.start()
+                    vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                    vim.wo[0][0].foldmethod = 'expr'
+                end,
             })
         end
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        branch = "main",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        config = function()
+            require("nvim-treesitter-textobjects").setup({
+                select = {
+                    lookahead = true,
+                },
+            })
+
+            local select = require("nvim-treesitter-textobjects.select")
+            vim.keymap.set({ "x", "o" }, "af", function()
+                select.select_textobject("@function.outer", "textobjects")
+            end, { desc = "Select outer function" })
+            vim.keymap.set({ "x", "o" }, "if", function()
+                select.select_textobject("@function.inner", "textobjects")
+            end, { desc = "Select inner function" })
+        end,
     },
     {
         'aaronik/treewalker.nvim',
